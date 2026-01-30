115217b0e2f6b7b86850c08b952c62550b1e57e4
Killian <killian.delamarre-ruer@students.dominican.edu>
2026-01-29 19:05:51 -0800

    all sorting done

--- sorting_integer.py
+++ sorting_integer.py
@@ -37,11 +37,44 @@ def counting_sort(numbers):
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
-    # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    TODO: Running time: O(n+k) average case when items are distributed are evenely across the buckets. 
+    Wosrt case is O(n^2) if all items are in one bucket
+    TODO: Memory usage: O(n+k) where n is for storing the items in buckets and k is the number of buckets"""
+    
+    #check if list is empty or has element
+    if len(numbers) <= 1:
+        return
+
+    #find range of given number min max values
+    min_val = min(numbers)
+    max_val = max(numbers)
+
+    #handle adge case where all numbers are same
+    if min_val == max_val:
+        return
+
+    #create list of buckets to store numbers in subranges on input range
+    buckets = [[] for _ in range(num_buckets)]
+
+    #calc range of each bucket
+    range_size = (max_val - min_val) / num_buckets
+
+    #loop over given numbers and place each item in appropriate bucket
+    for num in numbers:
+        #dtermine which bucksts number belongs to
+        bucket_index = int((num - min_val) / range_size)
+        #handle edge case where num == max_val
+        if bucket_index == num_buckets:
+            bucket_index = num_buckets - 1
+        buckets[bucket_index].append(num)
+
+    #sort each bucket using ANY sortign algo
+    for bucket in buckets:
+        insertion_sort(bucket)
+    
+    #loop over buckets and appench each buckets numbers into ouput list
+    index = 0
+    for bucket in buckets:
+        for num in buckets:
+            numbers[index] = num
+            index += 1
